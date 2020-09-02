b8b1be35d5c2ad2608e95cab3a2734a02e65f9aa
Bingyang Xu <hi@xubingyang.com>
2020-09-02 13:36:38 +0900

    惊雷一声，应用完成。

--- index.js
+++ index.js
@@ -75,34 +75,47 @@ const today = moment().locale('zh-cn').format('YYYY-MM-DD');
       await driver.findElement(By.linkText('口座一覧')).click();
       UFJAssets = await (await driver.findElement(By.css('#remainder_info > div > div > div > div.flat_unit.head_info > div.fleft > div > div > table > tbody > tr > td.number > strong'))).getText();
       UFJAssets = Number(UFJAssets.replace(regex, ''));
-      UFJAssetsFutsu = await (await driver.findElement(By.css('#remainder_info > div > div > div > div.info_table_gray.section > table > tbody > tr > td.balance_info > p > strong'))).getText();
-      UFJAssetsFutsu = Number(UFJAssetsFutsu.replace(regex, ''));
-      UFJAssetsTeiki = await (await driver.findElement(By.css('#remainder_info > div > div > div > div:nth-child(5) > div:nth-child(2) > div > div.read_first_table > table > tbody > tr > td.balance_info > p > strong'))).getText();
-      UFJAssetsTeiki = Number(UFJAssetsTeiki.replace(regex, ''));
-      UFJAssetsGaikaFutsu = await (await driver.findElement(By.css('#remainder_info > div > div > div > div:nth-child(5) > div:nth-child(3) > div > div.read_first_table > table > tbody > tr.first_child > td.balance_info > p > strong'))).getText();
-      UFJAssetsGaikaFutsu = Number(UFJAssetsGaikaFutsu.replace(regex, ''));
-      UFJAssetsGaikaTeiki = await (await driver.findElement(By.css('#remainder_info > div > div > div > div:nth-child(5) > div:nth-child(3) > div > div.read_first_table > table > tbody > tr:nth-child(2) > td.balance_info > p > strong'))).getText();
-      UFJAssetsGaikaTeiki = Number(UFJAssetsGaikaTeiki.replace(regex, ''));
-      UFJAssetsGaikaCyochiku = await (await driver.findElement(By.css('#remainder_info > div > div > div > div:nth-child(5) > div:nth-child(3) > div > div.read_first_table > table > tbody > tr:nth-child(3) > td.balance_info > p > strong'))).getText();
-      UFJAssetsGaikaCyochiku = Number(UFJAssetsGaikaCyochiku.replace(regex, ''));
-      await driver.findElement(By.linkText('投資信託トップ')).click();
-      await driver.sleep(process.env.WAIT_INTERVAL);
-      UFJAssetsShintaku = await (await driver.findElement(By.css('#fundlist > div > div > div > div.head_info > div.colR > dl > dd.item01 > strong'))).getText();
-      UFJAssetsShintaku = Number(UFJAssetsShintaku.replace(regex, ''));
-      UFJAssetsShintakuRevenue = await (await driver.findElement(By.css('#fundlist > div > div > div > div.head_info > div.colR > dl > dd.item02 > strong'))).getText();
-      if (UFJAssetsShintakuRevenue.startsWith('+')) {
-        UFJAssetsShintakuRevenue = Number(UFJAssetsShintakuRevenue.substring(1, UFJAssetsShintakuRevenue.length).replace(regex, ''));
-      } else {
-        UFJAssetsShintakuRevenue = Number(UFJAssetsShintakuRevenue.substring(1, UFJAssetsShintakuRevenue.length).replace(regex, '')) * -1;
-      }
       console.log('三菱UFJ銀行总资产：', colors.red(UFJAssets));
-      console.log('三菱UFJ銀行普通預金账户资产：', colors.red(UFJAssetsFutsu));
-      console.log('三菱UFJ銀行定期預金账户资产：', colors.red(UFJAssetsTeiki));
-      console.log('三菱UFJ銀行外貨普通账户资产：', colors.red(UFJAssetsGaikaFutsu));
-      console.log('三菱UFJ銀行外貨定期账户资产：', colors.red(UFJAssetsGaikaTeiki));
-      console.log('三菱UFJ銀行外貨貯蓄账户资产：', colors.red(UFJAssetsGaikaCyochiku));
-      console.log('三菱UFJ銀行投資信託账户资产：', colors.red(UFJAssetsShintaku));
-      console.log('三菱UFJ銀行投資信託账户收益：', colors.red(UFJAssetsShintakuRevenue));
+      if (driver.findElement(By.css('#remainder_info > div > div > div > div.info_table_gray.section > table > tbody > tr > td.balance_info > p > strong')) != null) {
+        UFJAssetsFutsu = await (await driver.findElement(By.css('#remainder_info > div > div > div > div.info_table_gray.section > table > tbody > tr > td.balance_info > p > strong'))).getText();
+        UFJAssetsFutsu = Number(UFJAssetsFutsu.replace(regex, ''));
+        console.log('三菱UFJ銀行普通預金账户资产：', colors.red(UFJAssetsFutsu));
+      }
+      if (driver.findElement(By.css('#remainder_info > div > div > div > div:nth-child(5) > div:nth-child(2) > div > div.read_first_table > table > tbody > tr > td.balance_info > p > strong')) != null) {
+        UFJAssetsTeiki = await (await driver.findElement(By.css('#remainder_info > div > div > div > div:nth-child(5) > div:nth-child(2) > div > div.read_first_table > table > tbody > tr > td.balance_info > p > strong'))).getText();
+        UFJAssetsTeiki = Number(UFJAssetsTeiki.replace(regex, ''));
+        console.log('三菱UFJ銀行定期預金账户资产：', colors.red(UFJAssetsTeiki));
+      }
+      if (driver.findElement(By.css('#remainder_info > div > div > div > div:nth-child(5) > div:nth-child(3) > div > div.read_first_table > table > tbody > tr.first_child > td.balance_info > p > strong')) != null) {
+        UFJAssetsGaikaFutsu = await (await driver.findElement(By.css('#remainder_info > div > div > div > div:nth-child(5) > div:nth-child(3) > div > div.read_first_table > table > tbody > tr.first_child > td.balance_info > p > strong'))).getText();
+        UFJAssetsGaikaFutsu = Number(UFJAssetsGaikaFutsu.replace(regex, ''));
+        console.log('三菱UFJ銀行外貨普通账户资产：', colors.red(UFJAssetsGaikaFutsu));
+      }
+      if (driver.findElement(By.css('#remainder_info > div > div > div > div:nth-child(5) > div:nth-child(3) > div > div.read_first_table > table > tbody > tr:nth-child(2) > td.balance_info > p > strong')) != null) {
+        UFJAssetsGaikaTeiki = await (await driver.findElement(By.css('#remainder_info > div > div > div > div:nth-child(5) > div:nth-child(3) > div > div.read_first_table > table > tbody > tr:nth-child(2) > td.balance_info > p > strong'))).getText();
+        UFJAssetsGaikaTeiki = Number(UFJAssetsGaikaTeiki.replace(regex, ''));
+        console.log('三菱UFJ銀行外貨定期账户资产：', colors.red(UFJAssetsGaikaTeiki));
+      }
+      if (driver.findElement(By.css('#remainder_info > div > div > div > div:nth-child(5) > div:nth-child(3) > div > div.read_first_table > table > tbody > tr:nth-child(3) > td.balance_info > p > strong')) != null) {
+        UFJAssetsGaikaCyochiku = await (await driver.findElement(By.css('#remainder_info > div > div > div > div:nth-child(5) > div:nth-child(3) > div > div.read_first_table > table > tbody > tr:nth-child(3) > td.balance_info > p > strong'))).getText();
+        UFJAssetsGaikaCyochiku = Number(UFJAssetsGaikaCyochiku.replace(regex, ''));
+        console.log('三菱UFJ銀行外貨貯蓄账户资产：', colors.red(UFJAssetsGaikaCyochiku));
+      }
+      if (driver.findElement(By.linkText('投資信託トップ')) != null) {
+        await driver.findElement(By.linkText('投資信託トップ')).click();
+        await driver.sleep(process.env.WAIT_INTERVAL);
+        UFJAssetsShintaku = await (await driver.findElement(By.css('#fundlist > div > div > div > div.head_info > div.colR > dl > dd.item01 > strong'))).getText();
+        UFJAssetsShintaku = Number(UFJAssetsShintaku.replace(regex, ''));
+        console.log('三菱UFJ銀行投資信託账户资产：', colors.red(UFJAssetsShintaku));
+        UFJAssetsShintakuRevenue = await (await driver.findElement(By.css('#fundlist > div > div > div > div.head_info > div.colR > dl > dd.item02 > strong'))).getText();
+        if (UFJAssetsShintakuRevenue.startsWith('+')) {
+          UFJAssetsShintakuRevenue = Number(UFJAssetsShintakuRevenue.substring(1, UFJAssetsShintakuRevenue.length).replace(regex, ''));
+        } else {
+          UFJAssetsShintakuRevenue = Number(UFJAssetsShintakuRevenue.substring(1, UFJAssetsShintakuRevenue.length).replace(regex, '')) * -1;
+        }
+        console.log('三菱UFJ銀行投資信託账户收益：', colors.red(UFJAssetsShintakuRevenue));
+      }
+
       allAssets += UFJAssets;
     }
 
@@ -115,22 +128,21 @@ const today = moment().locale('zh-cn').format('YYYY-MM-DD');
       await driver.sleep(process.env.WAIT_INTERVAL);
       NomuraAssets = await (await driver.findElement(By.css('#main > div > div > div.ct-top-col-left > div.asset-summary-set > div > dl > dd > div > div.ct-top-asset-col-left.grid-15.grid-sd-24.valign-t > div > div > table > tbody > tr:nth-child(1) > td > strong'))).getText();
       NomuraAssets = Number(NomuraAssets.replace(regex, ''));
+      console.log('野村證券总资产: ', colors.yellow(NomuraAssets));
       NomuraAssetsRevenue = await (await driver.findElement(By.css('#main > div > div > div.ct-top-col-left > div.asset-summary-set > div > dl > dd > div > div.ct-top-asset-col-left.grid-15.grid-sd-24.valign-t > div > div > table > tbody > tr:nth-child(2) > td > span'))).getText();
       NomuraAssetsRevenue = Number(NomuraAssetsRevenue.replace(regex, ''));
+      console.log('野村證券账户收益：', colors.yellow(NomuraAssetsRevenue));
       await driver.findElement(By.linkText('お預り資産を確認')).click();
       NomuraMRF = await (await driver.findElement(By.css('#mrf > div.hidden-sd > table > tbody > tr:nth-child(1) > td.txt-num.no-line-td-bottom'))).getText();
       NomuraMRF = Number(NomuraMRF.substring(0, NomuraMRF.length - 1).replace(regex, ''));
+      console.log('野村證券MRF账户资产：', colors.yellow(NomuraMRF));
       if (driver.findElement(By.css('#mrf > div.hidden-sd > table > tbody > tr:nth-child(3) > td:nth-child(1)'))) {
         NomuraTsumitate = await (await driver.findElement(By.css('#mrf > div.hidden-sd > table > tbody > tr:nth-child(3) > td.txt-num.no-line-td-bottom'))).getText();
         NomuraTsumitate = Number(NomuraTsumitate.substring(0, NomuraTsumitate.length - 1).replace(regex, ''));
+        console.log('野村證券投信積立账户资产：', colors.yellow(NomuraTsumitate));
       }
       NomuraTokutei = await (await driver.findElement(By.css('#domestic-trust > div:nth-child(2) > table > tbody > tr > td:nth-child(2)'))).getText();
       NomuraTokutei = Number(NomuraTokutei.substring(0, NomuraTokutei.length - 1).replace(regex, ''));
-
-      console.log('野村證券总资产: ', colors.yellow(NomuraAssets));
-      console.log('野村證券账户收益：', colors.yellow(NomuraAssetsRevenue));
-      console.log('野村證券MRF账户资产：', colors.yellow(NomuraMRF));
-      console.log('野村證券投信積立账户资产：', colors.yellow(NomuraTsumitate));
       console.log('野村證券特定預り账户资产：', colors.yellow(NomuraTokutei));
 
       allAssets += NomuraAssets;
@@ -144,21 +156,21 @@ const today = moment().locale('zh-cn').format('YYYY-MM-DD');
       await driver.sleep(process.env.WAIT_INTERVAL);
       RakutenCreditCardDebt = await (await driver.findElement(By.css('#js-bill-mask > em'))).getText();
       RakutenCreditCardDebt = Number(RakutenCreditCardDebt.substring(0, RakutenCreditCardDebt.length - 1).replace(regex, ''));
+      console.log(`楽天カード${RakutenCreditCardPaymentInfo}: `, colors.gray(RakutenCreditCardDebt));
       RakutenCreditCardDebtPayDate = await (await driver.findElement(By.css('#top > div.rce-l-wrap.is-grey.rce-main > div > div.rce-billInfo.rf-card.rf-card-square.rf-card-edge > div.rce-contents > div.rce-columns > div.rce-columns-cell.rce-billInfo-month > table:nth-child(2) > tbody > tr:nth-child(1) > td > em'))).getText();
       RakutenCreditCardDebtPayDate = Number(RakutenCreditCardDebtPayDate.substring(0, RakutenCreditCardDebtPayDate.length - 5).replace('年', '').replace('月', '').replace('日', ''));
+      console.log('楽天カードお支払い日: ', colors.gray(RakutenCreditCardDebtPayDate));
       RakutenCreditCardDebtAvailable = await (await driver.findElement(By.id('js-bill-available'))).getText();
       RakutenCreditCardDebtAvailable = Number(RakutenCreditCardDebtAvailable.trim().substring(0, RakutenCreditCardDebtAvailable.length - 1).replace(regex, ''));
+      console.log('楽天カード現在のご利用可能額: ', colors.gray(RakutenCreditCardDebtAvailable));
       RakutenCreditCardDebtTotal = await (await driver.findElement(By.id('js-bill-available-amount'))).getText();
       RakutenCreditCardDebtTotal = Number(RakutenCreditCardDebtTotal.trim().substring(0, RakutenCreditCardDebtTotal.length - 1).replace(regex, ''));
       RakutenPoints = await (await driver.findElement(By.css('#pointInformation > div > div.rce-membership-point > dl > dd.rce-point-all > strong'))).getText();
+      console.log('楽天カードご利用可能枠: ', colors.gray(RakutenCreditCardDebtTotal));
       RakutenPoints = Number(RakutenPoints.trim().replace(regex, ''));
       RakutenPointsLimited = await (await driver.findElement(By.css('#pointInformation > div > div.rce-membership-point > dl > dd:nth-child(4) > strong'))).getText();
       RakutenPointsLimited = Number(RakutenPointsLimited.trim().replace(regex, ''));
       const RakutenCreditCardPaymentInfo = await (await driver.findElement(By.css('#top > div.rce-l-wrap.is-grey.rce-main > div > div.rce-billInfo.rf-card.rf-card-square.rf-card-edge > div.rce-contents > div.rce-columns > div.rce-columns-cell.rce-billInfo-month > h3.rf-title-collar.rce-title-belt-first'))).getText();;
-      console.log(`楽天カード${RakutenCreditCardPaymentInfo}: `, colors.gray(RakutenCreditCardDebt));
-      console.log('楽天カードお支払い日: ', colors.gray(RakutenCreditCardDebtPayDate));
-      console.log('楽天カード現在のご利用可能額: ', colors.gray(RakutenCreditCardDebtAvailable));
-      console.log('楽天カードご利用可能枠: ', colors.gray(RakutenCreditCardDebtTotal));
 
       allDebts += RakutenCreditCardDebt;
       allPoints += RakutenPoints;
